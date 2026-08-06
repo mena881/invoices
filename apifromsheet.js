f0def4c633406827cfde7b36933eba8e44066c9f
Mena Sameh <menafx45@gmail.com>
2026-08-06 14:09:41 +0300

    Update apifromsheet.js

--- apifromsheet.js
+++ apifromsheet.js
@@ -1,8 +1,8 @@
 // apifromsheet.js
 
 // دالة لجلب البيانات من Web App
-async function fetchInventoryData() {
-    const url = 'https://script.google.com/macros/s/AKfycbw302fPZAN0hVwn4QGaIR18LDtoj0bI97zsLT8LhDqwO52OP_APfheG49n2ECxYD3xcVg/exec?action=read&sheet=%D8%A7%D8%B3%D8%AA%D9%88%D9%83';
+async function fetchInventoryData(sheetName) {
+    const url = `https://script.google.com/macros/s/AKfycbw302fPZAN0hVwn4QGaIR18LDtoj0bI97zsLT8LhDqwO52OP_APfheG49n2ECxYD3xcVg/exec?action=read&sheet=${encodeURIComponent(sheetName)}`;
     try {
         const response = await fetch(url);
         if (!response.ok) {
